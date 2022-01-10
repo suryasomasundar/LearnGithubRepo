7cb0d8e472e8114b8d56591dbe2482b6c38a70d9
Somu <somu@somu-macp.myvest.com>
2022-01-09 16:12:59 -0800

    Test2- Updating the commits

--- testGitHub.sh
+++ testGitHub.sh
@@ -1,3 +1,7 @@
 Test1 - Learn Basic Add, Commit and Push to Master
 Test2 - Update the same file and push 
 Test3 - Created a new Branch in Git and Local 
+Commands 
+git branch -r // to see the list of branches
+git checkout -b <branchName> origin/<RemoteBranch> // Checkout a new branch
+git push origin HEAD:TestBranch // Push to remote branch
